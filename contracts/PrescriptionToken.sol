133be4999f29b6b16c227638ffd60862fb4ed5ec
Ramon Silvente Galera <rsilgal@gmail.com>
2023-06-26 12:42:25 +0200

    [ADD] Prescriptoin structure, modifier and get functions

--- contracts/PrescriptionToken.sol
+++ contracts/PrescriptionToken.sol
@@ -8,22 +8,43 @@ import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract PrescriptionToken is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
+contract PrescriptionToken is
+    ERC721,
+    ERC721Enumerable,
+    Pausable,
+    AccessControl,
+    ERC721Burnable
+{
     using Counters for Counters.Counter;
+
+    struct Prescription {
+        uint256 productId;
+        uint256 amountToTake;
+        uint256 coolDownHours;
+        uint256 productQuantity;
+        address to;
+    }
+
+    mapping (uint => Prescription) prescriptions;
 
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    uint256 public productId;
-    uint256 public amountToTake;
-    uint256 public coolDownHours; 
-    uint256 public productQuantity;
-
     constructor() ERC721("PrescriptionToken", "MTK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    modifier _checkIfItExist(uint256 tokenId) {
+        require(_exists(tokenId), "Not exist a token with this Id");
+        _;
+    }
+
+    modifier _hasPermision(uint256 tokenId){
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender) || ownerOf(tokeId) === msg.sender, "Can't read this prescription");
+        _;
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -34,27 +55,33 @@ contract PrescriptionToken is ERC721, ERC721Enumerable, Pausable, AccessControl,
         _unpause();
     }
 
-    function safeMint(address to, uint256 _productId, uint256 _amountToTake, uint256 _coolDownHours, uint256 _productQuantity) public onlyRole(MINTER_ROLE) {
-        productId = _productId;
-        amountToTake = _amountToTake;
-        coolDownHours = _coolDownHours;
-        productQuantity = _productQuantity;
+    function safeMint(
+        address to,
+        uint256 _productId,
+        uint256 _amountToTake,
+        uint256 _coolDownHours,
+        uint256 _productQuantity
+    ) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
+        prescriptions[tokenId] = new Prescription(_productId, _amountToTake, _coolDownHours, _productQuantity, to)
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         override(ERC721, ERC721Enumerable, AccessControl)
@@ -63,13 +90,25 @@ contract PrescriptionToken is ERC721, ERC721Enumerable, Pausable, AccessControl,
         return super.supportsInterface(interfaceId);
     }
 
-        function grantRoleMinter(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function grantRoleMinter(
+        address account
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(MINTER_ROLE, account);
         // TODO: Emit an event
     }
 
-    function grantRoleAdmin(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function grantRoleAdmin(
+        address account
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _grantRole(DEFAULT_ADMIN_ROLE, account);
         //TODO: Emit an event
+    }
+
+    function _get(uint256 tokenId) internal view _checkIfItExist(tokenId) _hasPermision(tokeId) returns (Prescription){
+        return prescriptions[tokenId];
+    }
+
+    function get(uint256 tokenId) external view {
+        return _get(uint256 tokenId);
     }
 }
